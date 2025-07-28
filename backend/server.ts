43075cd08b1b59549382021e0b4f036c810d56e9
Atharv Atre <atharv@velnir.com>
2025-07-28 21:58:47 +0530

    add server.js

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import todoRoutes from "./routes/todos";
+import todoRoutes from "./src/routes/todos";
 
 dotenv.config();
 
